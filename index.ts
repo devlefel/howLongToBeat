3c36299a34034e592a84da89c2c96573bf562cd6
Lefel95 <felipe.pbgomes@gmail.com>
2023-05-19 19:23:01 -0300

    make tests and added ts

--- index.ts
+++ index.ts
@@ -1,7 +1,26 @@
 import { HowLongToBeatService, HowLongToBeatEntry } from "howlongtobeat";
 
+type Hours = {
+  MinHours: number
+  MaxHours: number
+  Medium: number
+}
 
+type HLTBRunner = {
+  Parser,
+  Writter,
+  Reader,
+  Search: (name: string) => Hours,
+}
+
+let runner : HLTBRunner
 
 let hltbService = new HowLongToBeatService();
+const search = (name: string) => (Hours) => {
+
+};
+runner.Search = (name: string) => Hours {
+
 
 hltbService.search('nioh').then(result => console.log(result));
+};
