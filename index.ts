265fa6296a334708018b165dc8d020435ff3764a
Lefel95 <felipe.pbgomes@gmail.com>
2023-05-21 23:25:16 -0300

    added csv-parser

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import { HowLongToBeatService, HowLongToBeatEntry } from "howlongtobeat";
 import { GameHour } from "./models/Classes";
+import * as fs from "fs";
+import { parse } from "csv/sync";
 
 class service {
     private howLongToBeatService: HowLongToBeatService;
     constructor(howLongToBeatService: HowLongToBeatService) {
         this.howLongToBeatService = howLongToBeatService;
+    }
+
+    async read(path: string) {
+        const fileContent = await fs.promises.readFile(path, "utf8");
+        const records = parse(fileContent, { columns: true });
+        for (const record of records) {
+            await this.search(record.Name);
+        }
     }
 
     async search(name: string): Promise<GameHour> {
@@ -19,6 +29,6 @@ class service {
 const hltbs = new service(new HowLongToBeatService)
 
 
-hltbs.search('nioh')
+hltbs.read('./read_fs.csv')
     .then(result => console.log('feito!'))
     .catch(err => console.log(`err ${err}`));
